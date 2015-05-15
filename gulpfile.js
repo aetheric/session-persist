34085059b2dbe04ad8ca7f847042269d8d7545ea
pcummuskey <pcummuskey@bravurasolutions.com>
2015-05-15 12:50:01 +1200

    Fixing package command and renaming project again.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 /* global require */
 
-
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 gulp.task('test', function() {
-	//
+	return gulp.src('src/test/**/*.spec.js');
 });
 
-gulp.task('pack', function() {
-	//
+gulp.task('dist', function() {
+	return gulp.src('src/main/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
 });
 
